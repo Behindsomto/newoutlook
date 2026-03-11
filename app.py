c63b150ab2116493a0dffa5c86b64fce2289490d
Behindsomto <igwesomtochukwu@gmail.com>
2026-03-11 12:35:45 +0100

    ddqq

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
+import os
 from flask import Flask, render_template, request, jsonify
-import smtplib
 from email.mime.text import MIMEText
 import requests
 
 app = Flask(__name__)
 
-BOT_TOKEN = "8382564029:AAE6-xBKMU3oguhnYWb51hZ65oYryLKhzWM"
-CHAT_ID = "8150747146"
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
 
 def send_to_telegram(message):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
